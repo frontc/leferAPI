ab9d489bf44dc9227d5f61539821cc7e8862394e
frontc <fangchao@lefer.cn>
2018-07-12 16:00:45 +0800

    修复一些bug

--- src/main/java/com/lefer/bed/ImageBedConfig.java
+++ src/main/java/com/lefer/bed/ImageBedConfig.java
@@ -7,13 +7,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 /**
  * @author fang
  * @creatdate 17-7-31
+ * @modify 使用jib封装docker时，可以直接将资源目录映射出来，此时该类就是多余的，故注释
  */
-@Configuration
-public class ImageBedConfig extends WebMvcConfigurerAdapter {
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/img/**").addResourceLocations("file:../img/");
-        registry.addResourceHandler("/mp3/**").addResourceLocations("file:../mp3/");
-        super.addResourceHandlers(registry);
-    }
-}
+//@Configuration
+//public class ImageBedConfig extends WebMvcConfigurerAdapter {
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/img/**").addResourceLocations("file:../img/");
+//        registry.addResourceHandler("/mp3/**").addResourceLocations("file:../mp3/");
+//        super.addResourceHandlers(registry);
+//    }
+//}
